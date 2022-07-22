7e694aa557b5ae567ec406d5d51c2711ab8522cb
Saaim-K <saaimkhan221@gmail.com>
2022-07-22 06:17:01 +0500

    55%

--- app.js
+++ app.js
@@ -88,3 +88,37 @@ function onError(error) {
 //-------------------- onError Parameter --------------------
 
 //-------------------------------------------------- Weather Using Location --------------------------------------------------
+
+
+//-------------------------------------------------- Weather Using Search --------------------------------------------------
+function getWeather() {
+    let input_city = document.getElementById('input_city').value;
+    console.log(input_city);
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=92203d8e3c314335b7462722223006&q=${input_city}`)
+        .then(function (response) {
+            console.log(response.data);
+            document.getElementById('city').innerHTML = response.data.location.name;
+            document.getElementById('country').innerHTML = response.data.location.country + "<br>";
+            document.getElementById('temp-f').innerHTML = response.data.current.temp_c + "°C" + "<br>";
+        })
+    access.innerHTML = "";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------- Weather Using Search --------------------------------------------------
